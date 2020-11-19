3d0bc8039ab5261398fdb78f77a66f9864c6bea1
null-bit1978 <routerhosting1978@protonmail.com>
2020-11-19 15:08:29 -0500

    final provisional version

--- recon.py
+++ recon.py
@@ -5,8 +5,11 @@ import sys
 import re
 import nmap
 import dns.resolver
+import requests
+from shodan import Shodan
 
 def Main():
+    IS_HTTP = False # esta variable la utilizamos luego para analizar la parte de servidor web
     # parseamos las opciones y argumentos. El único argumento obligatorio es la de host. La opción de --nmap se usa para cumplir con el punto 2 de la práctica
     parser = argparse.ArgumentParser()
     parser.add_argument("host", help="The host to be recon", type=str)
@@ -33,6 +36,15 @@ def Main():
         nm.scan(hosts=host, arguments="--top-ports 1000 -sV -T4")
         return nm[host]['tcp'].items()
 
+    def shodan_scan(host):
+        key = "TMOFqYjmOcTWqhFSGZ2qIA4Xo72hZxfX"
+        engine = Shodan(key)
+        return engine.host(host)
+
+    def options_request(host):
+        verbs = requests.options(host)
+        return verbs.headers
+
     # para poder compaginar toda la funcionalidad del script, necesitamos uniformizar el formato del host, por lo que verificamos, con ayuda de nuestro patrón,
     # si hemos recibido una dirección IP o un nombre de dominio
     if re.match(ip_pattern, args.host):
@@ -55,16 +67,16 @@ def Main():
     except whois.exceptions.UnknownTld:
         print("No hay registros whois para el TLD solicitado")
     try:
-        regs = ["A", "NS", "MX", "CNAME", "TXT"]
+        regs = ["A", "NS", "MX", "AAAA", "TXT"]
         dns_info = map(get_dns_info, regs) # usamos map para pasar todos los valores de la lista a nuestra función y evitarnos escribir más código
         print("---------------------------------")
         print(f"The DNS info for {host} is:")
         for elem in dns_info:
             print(elem)
     except:
-        print(f"Couldn't get the info requested for this register")
-        
-    # chequeamos si la flag de nma está activada
+        print(f"Couldn't get the info requested for this register.")
+
+    # chequeamos si la flag de nmap está activada
     if args.nmap:
         host = socket.gethostbyname(args.host)
         print("-------------------------------------")
@@ -74,9 +86,30 @@ def Main():
         for port, value in ports:
             print(str(port) + ": " + value['state'] + " " + value['product'] + " " + value['version'])
 
+        print("-------------------------------------")
+        print("Scanning the host with shodan, please wait...")
 
+        shodan_info = shodan_scan(host)
+        www_ports = [80, 8080, 443]
+        for k, v in shodan_info.items():
+            print(str(k) + ": " +str(v))
+        shodan_ports = shodan_info['ports']
+        for port in shodan_ports:
+            if port in www_ports:
+                IS_HTTP = True
+        if IS_HTTP:
+            try:
+                print("------------------------------------")
+                headers = options_request("http://" + host)
+                print("The HTTP methods allow for this host are:")
+                print(headers['allow'])
+            except KeyError:
+                print("The server didn't return the 'allow' header")
+                print("These are the headers returned by the server:")
+                print(headers)
 
-
+        else:
+            print("The host is not an HTTP server.")
 
 
 if __name__ == "__main__":
